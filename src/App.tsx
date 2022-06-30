bad77ee000bc9bdf2ae9f94abc3b69a62177410c
gizeasy <gizeasy@gmail.com>
2022-06-30 12:57:10 +0300

    adaptive and header

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,18 @@ import { Pages } from '##/containers/Pages';
 import { Menu } from '##/containers/Menu';
 import { SideLinks } from '##/componets/SideLinks';
 import { useMenu } from '##/hooks/useMenu';
+import { RightMenu } from '##/containers/RightMenu';
+import { Header } from '##/containers/Header';
 
 export const App: React.FC = () => {
   const { menu, links, showMenu } = useMenu();
 
   return (
-    <DocLayout leftSide={<Menu />} rightSide={showMenu ? <SideLinks menu={menu} links={links} /> : undefined}>
+    <DocLayout
+      leftSide={<Menu />}
+      rightSide={showMenu ? <SideLinks menu={menu} links={links} /> : undefined}
+      header={<Header />}
+    >
       <Pages />
     </DocLayout>
   );
